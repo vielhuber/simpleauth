2b6e98b6c6560282d50e695a21361d469a6422b8
David Vielhuber <david@vielhuber.de>
2016-09-08 22:16:07 +0200

    clean up

--- init.php
+++ init.php
@@ -1,6 +1,28 @@
 <?php
 require_once('functions.php');
 
-auth_init_tables();
-create_user('david','123');
-die('ok');
+/* init tables */
+db_query("
+	CREATE TABLE `users` (
+		`id`			bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+		`username`		varchar(32) NOT NULL,
+		`password`		char(40) NOT NULL,
+		`salt`			char(32) NOT NULL,
+		`email`			varchar(100) NOT NULL,
+		PRIMARY KEY (`id`)
+	);
+");
+db_query("
+	CREATE TABLE `auth` (
+		`id`			bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+		`token`			char(32) NOT NULL,
+		`valid_until`	datetime NOT NULL,
+		`user_id`		bigint(20) UNSIGNED NOT NULL,
+		PRIMARY KEY (`id`),
+		FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT, INDEX `user_id` (`user_id`) USING BTREE
+	);
+");
+ 
+/* create user */
+auth_create_user('david','123','david@vielhuber.de');
+
--- login_controller.php
+++ login_controller.php
@@ -1,4 +1,4 @@
 <?php
 require_once('functions.php');
 
-auth_login_controller();
+auth_login_controller($_POST["username"], $_POST["password"]);
