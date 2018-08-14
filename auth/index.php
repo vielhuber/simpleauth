30ea8e4d1ffe993792ad11ed822ff27744c08b53
David Vielhuber <david@vielhuber.de>
2018-08-14 02:22:46 +0200

    Include composer in any subpath

--- auth/index.php
+++ auth/index.php
@@ -1,5 +1,14 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+$i = 0;
+while($i < 5)
+{
+    if( file_exists( __DIR__.'/'.str_repeat('../',$i).'vendor/autoload.php' ) )
+    {
+        require_once __DIR__.'/'.str_repeat('../',$i).'vendor/autoload.php';
+        break;
+    }
+    $i++;
+}
 require_once __DIR__.'/config.php';
 use vielhuber\simpleauth\simpleauth;
 $auth = new simpleauth($config);
