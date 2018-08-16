fd0900ccd691feee4cf44c6f26ef4e72a779b74c
David Vielhuber <david@vielhuber.de>
2018-08-17 01:57:30 +0200

    Fixed demo

--- demo/includes.php
+++ demo/includes.php
@@ -2,16 +2,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 use vielhuber\simpleauth\simpleauth;
 
-$auth = new simpleauth([
-    'dbms' => 'mysql',
-    'host' => '127.0.0.1',
-    'username' => 'root',
-    'password' => 'root',
-    'database' => 'simpleauth',
-    'table' => 'users',
-    'port' => 3306,
-    'ttl' => 30
-]);
+$auth = new simpleauth(__DIR__.'/../.env');
 
 try {
     $auth->createTable();
