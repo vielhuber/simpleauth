b535ea7d055e52920c7c744d8d18748b6dd91c9e
David Vielhuber <david@vielhuber.de>
2018-08-17 01:45:42 +0200

    Add content type to response

--- src/simpleauth.php
+++ src/simpleauth.php
@@ -392,6 +392,7 @@ class simpleauth
     private function apiResponse($data, $code = 200)
     {
         http_response_code($code);
+        header('Content-Type: application/json');
         echo json_encode($data);
         die();
     }
