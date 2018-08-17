78985793b6a2551748912715c1657a37c068b100
David Vielhuber <david@vielhuber.de>
2018-08-18 01:28:30 +0200

    Improved input handling

--- src/simpleauth.php
+++ src/simpleauth.php
@@ -106,14 +106,25 @@ class simpleauth
 
     private function apiInput($key)
     {
-        global $_POST;
         $p1 = $_POST;
         $p2 = json_decode(file_get_contents('php://input'), true);
-        if (isset($p1) && !empty($p1) && array_key_exists($key, $p1)) {
+        parse_str(file_get_contents('php://input'), $p3);
+        if( isset($p1) && !empty($p1) && array_key_exists($key, $p1) ) {
             return $p1[$key];
         }
-        if (isset($p2) && !empty($p2) && array_key_exists($key, $p2)) {
+        if( isset($p2) && !empty($p2) && array_key_exists($key, $p2) ) {
             return $p2[$key];
+        }        
+        if( isset($p3) && !empty($p3) )
+        {
+            foreach($p3 as $p3__key => $p3__value)
+            {
+                unset($p3[$p3__key]);
+                $p3[str_replace('amp;', '', $p3__key)] = $p3__value;
+            }
+            if( array_key_exists($key, $p3) ) {
+                return $p3[$key];
+            }
         }
         return null;
     }
