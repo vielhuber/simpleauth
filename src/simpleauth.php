f76f6208dafe536ef96b7a0dfac322439dbcc11e
David Vielhuber <david@vielhuber.de>
2026-01-13 14:06:49 +0100

    Add header check.

--- src/simpleauth.php
+++ src/simpleauth.php
@@ -6,7 +6,7 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 // cors
-if (PHP_SAPI != 'cli' || strpos($_SERVER['argv'][0], 'phpunit') === false) {
+if (!headers_sent() && (PHP_SAPI != 'cli' || strpos($_SERVER['argv'][0], 'phpunit') === false)) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
